7a0175245e6274c098b7a5011c25fbf90a2a3ed8
Vanessa <vanessa.silva5205@gmail.com>
2024-02-24 13:10:36 -0300

    Desafio do dia 6. #7DaysOfCode

--- day5.js
+++ day5.js
@@ -1,39 +1,55 @@
-let frutas = []
-let doces = []
-let laticinios = []
-let congelados = []
-let graosCereais = []
+let listaDeCompras = {'frutas': [], 'doces': [], 'laticinios': [], 'congelados': [], 'graosCereais': []}
 while(true) {
-    const opcao = prompt("Você deseja adicionar uma comida na sua lista de compras? 1- Sim ou 2 - Não")
-    if(opcao == 1) {
+    const opcao = prompt("1 - Você deseja adicionar uma comida na sua lista de compras\n2 - Remover algum item da lista\n3 - Sair")
+   if(opcao == 1) {
         comida = prompt("Qual comida você deseja inserir? ")
         categoria = prompt("Em qual categoria essa comida se encaixa? 1 - frutas, 2 - doces, 3 - laticínios, 4 - congelados e 5 - Grãos e cereais")
         verificaCategoria(Number(categoria), comida)
+    } else if(opcao == 2) {
+        exibeListaDeCompras();
+        comida = prompt("Digite o nome da comida que vc deseja remover");
+        remover(comida);
     } else {
-        alert(`Frutas: ${frutas.join(', ')}\n Doces: ${doces.join(', ')}\n Laticínios: ${laticinios.join(', ')}\n Congelados: ${congelados.join(', ')}\n Grãos e Cereais: ${graosCereais.join(', ')}`)
+        exibeListaDeCompras();
         break;
     }
+}
+
+function exibeListaDeCompras() {
+    alert(`Frutas: ${listaDeCompras.frutas.join(', ')}\n Doces: ${listaDeCompras.doces.join(', ')}\n Laticínios: ${listaDeCompras.laticinios.join(', ')}\n Congelados: ${listaDeCompras.congelados.join(', ')}\n Grãos e Cereais: ${listaDeCompras.graosCereais.join(', ')}`)
 }
 
 function verificaCategoria(opcao, comida) {
    switch (opcao) {
     case 1:
-        frutas.push(comida)
+        listaDeCompras.frutas.push(comida)
         break;
     case 2:
-        doces.push(comida)
+        listaDeCompras.doces.push(comida)
         break;
     case 3:
-        laticinios.push(laticinios)
+        listaDeCompras.laticinios.push(comida)
         break
     case 4:
-        congelados.push(comida)
+        listaDeCompras.congelados.push(comida)
         break
     case 5:
-        graosCereais.push(comida)
+        listaDeCompras.graosCereais.push(comida)
         break
     default:
         alert("Não foi possível adicionar este alimento!");
         break;
    }
+}
+
+function remover(comida) {
+    for(let categoria in listaDeCompras) {
+        let index = listaDeCompras[categoria].indexOf(comida);
+        if (index !== -1) {
+            listaDeCompras[categoria].splice(index, 1);
+            alert(`${comida} foi removido da lista de compras.`);
+            return;
+        }
+    }
+    alert(`O item ${comida} não foi encontrado na lista.`);
 }
